1c251013ac6a2cdf909a6323443f65b882e20771
MahmoudELsaey <94616799+MahmoudELsaey@users.noreply.github.com>
2023-03-16 23:53:45 +0200

    Update main.c

--- Cat Task Weak 5 (Pointers)/Q(4)/main.c
+++ Cat Task Weak 5 (Pointers)/Q(4)/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ele 4 //we can change the size of the array.
+#define ele 7 //we can change the size of the array.
 
 int main()
 {
-    int arr [] = {20,10,500,1};
+    int arr [] = {20,10,500,1,25,15,5};
     BS(arr);
 
     return 0;
@@ -15,15 +15,15 @@ int main()
 
 int BS (int arr [])
 {
-    for (int j=0; j<ele; j++)
+    for (int i=0; i<ele-1; i++)
     {
-        for (int i=0; i<ele; i++)
+        for (int j=0; j<ele-i-1; j++)
         {
-            if (arr[j]>arr[j+i])
+            if (arr[j]>arr[j+1])
             {
                 int num = arr[j];
-                arr[j]=arr[j+i];
-                arr[j+i]= num;
+                arr[j]=arr[j+1];
+                arr[j+1]= num;
             }
         }
     }
