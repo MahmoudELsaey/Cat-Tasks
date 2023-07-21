196a7ea0a2123ea106fd3c6d706acad25065729d
Mahmoud Elsaey <94616799+MahmoudELsaey@users.noreply.github.com>
2023-07-21 15:43:48 +0300

    Delete project-1-main/project-1-main directory

--- project-1-main/project-1-main/student/student.h
+++
@@ -1,9 +0,0 @@
-#include"../database/database.h"
-// Fun of Viewing Records.
-void view_urecord(int id, student *students);
-
-// Fun Of Editting Password.
-void edit_upass (int id, student *students);
-
-// Fun Of Editting Name.
-void edit_uname (int id, student *students);
